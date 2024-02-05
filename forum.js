4ca3b862f21d2b83428c14f1455669713a06960d
Ben Khazaal <thajukez101@gmail.com>
2024-02-05 13:59:19 -0700

    fixed

--- forum.js
+++ forum.js
@@ -14,7 +14,7 @@ function SaveToText() {
 }
 
 function Blinking() {
-  var blinkingtext = document.getElementById("blinkerHeader");
+  var blinkingtext = document.getElementById("blinker");
   if (blinkingtext) {
     var span = document.createElement("span");
     span.textContent = blinkingtext.textContent;
@@ -27,3 +27,83 @@ function Blinking() {
   }
 }
 setInterval(Blinking, 1000);
+
+function Blinking2() {
+  var blinkingtext2 = document.getElementById("blinker2");
+  if (blinkingtext2) {
+    var span2 = document.createElement("span2");
+    span2.textContent = blinkingtext2.textContent;
+    span2.style.opacity = 1;
+    blinkingtext2.innerHTML = "";
+    blinkingtext2.appendChild(span2);
+    setTimeout(function () {
+      span2.style.opacity = 0;
+    }, 500);
+  }
+}
+
+setInterval(Blinking2, 1000);
+
+function Blinking3() {
+  var blinkingtext3 = document.getElementById("blinker3");
+  if (blinkingtext3) {
+    var span3 = document.createElement("span3");
+    span3.textContent = blinkingtext3.textContent;
+    span3.style.opacity = 1;
+    blinkingtext3.innerHTML = "";
+    blinkingtext3.appendChild(span3);
+    setTimeout(function () {
+      span3.style.opacity = 0;
+    }, 500);
+  }
+}
+
+setInterval(Blinking3, 1000);
+
+function Blinking4() {
+  var blinkingtext4 = document.getElementById("blinker4");
+  if (blinkingtext4) {
+    var span4 = document.createElement("span4");
+    span4.textContent = blinkingtext4.textContent;
+    span4.style.opacity = 1;
+    blinkingtext4.innerHTML = "";
+    blinkingtext4.appendChild(span4);
+    setTimeout(function () {
+      span4.style.opacity = 0;
+    }, 500);
+  }
+}
+
+setInterval(Blinking4, 1000);
+
+function Blinking5() {
+  var blinkingtext5 = document.getElementById("blinker5");
+  if (blinkingtext5) {
+    var span5 = document.createElement("span5");
+    span5.textContent = blinkingtext5.textContent;
+    span5.style.opacity = 1;
+    blinkingtext5.innerHTML = "";
+    blinkingtext5.appendChild(span5);
+    setTimeout(function () {
+      span5.style.opacity = 0;
+    }, 500);
+  }
+}
+
+setInterval(Blinking5, 1000);
+
+function Blinking6() {
+  var blinkingtext6 = document.getElementById("blinker6");
+  if (blinkingtext6) {
+    var span6 = document.createElement("span6");
+    span6.textContent = blinkingtext6.textContent;
+    span6.style.opacity = 1;
+    blinkingtext6.innerHTML = "";
+    blinkingtext6.appendChild(span6);
+    setTimeout(function () {
+      span6.style.opacity = 0;
+    }, 500);
+  }
+}
+
+setInterval(Blinking6, 1000);
